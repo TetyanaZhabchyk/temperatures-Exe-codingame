c1cf5b07b65a198ae171d6b007b44f2f7c453a0e
Chouteau <tetyana.chouteau@passculture.app>
2023-01-17 13:45:23 +0100

    todo, nearZero

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
 const n = parseInt(readline()); // the number of temperatures to analyse
-let nearZero = 5526
-var inputs = readline().split(' ');
+let nearZero = Number.POSITIVE_INFINITY;
+//if(nearZero === Number.POSITIVE_INFINITY)
+var inputs = readline().split(" ");
 for (let i = 0; i < n; i++) {
-    const t = parseInt(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526
-    if(Math.abs(t) < Math.abs(nearZero))
-        nearZero = t;
-    if(Math.abs(t) == Math.abs(nearZero))
-        if(t > 0)
-            nearZero = t;
+  const t = parseInt(inputs[i]); // a temperature expressed as an integer ranging from -273 to 5526
+  if (Math.abs(t) < Math.abs(nearZero)) nearZero = t;
+  if (Math.abs(t) == Math.abs(nearZero)) if (t > 0) nearZero = t;
 }
-
+console.error(inputs);
 // Write an answer using console.log()
 // To debug: console.error('Debug messages...');
-if(nearZero != 5526)
-    console.log(nearZero);
-else
-    console.log(0)
+if (inputs[0] === "") {
+  //[i] vide je log 0, else near zero)
+  console.log(0);
+} else {
+  console.log(nearZero);
+}
