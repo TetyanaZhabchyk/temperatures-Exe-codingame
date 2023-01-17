1dea998aee167fa15048dff38ce0f26a1a051135
Chouteau <tetyana.chouteau@passculture.app>
2023-01-17 10:40:52 +0100

    Ajout todo, ajout code

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const n = parseInt(readline()); // the number of temperatures to analyse
-let nearZero = 5526;
+**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
 
+const n = parseInt(readline()); // the number of temperatures to analyse
+let nearZero = 5526
+var inputs = readline().split(' ');
 for (let i = 0; i < n; i++) {
-  const t = parseInt(i); // a temperature expressed as an integer ranging from -273 to 5526
-  if (Math.abs(t) < Math.abs(nearZero)) nearZero = t;
-  if (Math.abs(t) == Math.abs(nearZero)) if (t > 0) nearZero = t;
+    const t = parseInt(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526
+    if(Math.abs(t) < Math.abs(nearZero))
+        nearZero = t;
+    if(Math.abs(t) == Math.abs(nearZero))
+        if(t > 0)
+            nearZero = t;
 }
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+if(nearZero != 5526)
+    console.log(nearZero);
+else
+    console.log(0)
